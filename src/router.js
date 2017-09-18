a2757672a078ca8513b4d7c27fa3cb4d7e76048a
Jeffrey Linwood <jlinwood@gmail.com>
2017-09-18 11:51:36 -0500

    Fix request/req typo

--- src/router.js
+++ src/router.js
@@ -47,4 +47,32 @@ router.get('/config', (req, res) => {
   res.json(config);
 });
 
+
+//Create a facebook-messenger binding based on the authentication webhook from Facebook
+router.post('/messenger_auth', function(req, res) {
+  //Extract the request received from Facebook
+  const message = req.body.entry[0].messaging[0];
+  console.log(message);
+  // Set user identity using their fb messenger user id
+  const identity = message.sender.id;
+  //Let's create a new facebook-messenger Binding for our user
+  const binding = {
+    "identity":identity,
+    "BindingType":'facebook-messenger',
+    "Address":message.sender.id
+  };
+  registerBind(camelCaseKeys(binding)).then((data) => {
+    res.status(data.status);
+    res.send(data.data);
+  });
+});
+
+//Verification endpoint for Facebook needed to register a webhook.
+router.get('/messenger_auth', function(req, res) {
+  console.log(req.query["hub.challenge"]);
+  res.send(req.query["hub.challenge"]);
+});
+
+
+
 module.exports = router;
