7983fd6fec95b9b9b2ec2da8a2e905440d0301c0
Jeffrey Linwood <jlinwood@gmail.com>
2017-10-23 16:52:21 -0500

    Fetch Sync Service on start

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const router = require('./src/router');
+const syncServiceDetails = require('./src/sync_service_details');
 
 // Create Express webapp
 const app = express();
@@ -17,6 +18,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(router);
+
+// Get Sync Service Details for lazy creation of default service if needed
+syncServiceDetails();
 
 // Create http server and run it
 const server = http.createServer(app);
